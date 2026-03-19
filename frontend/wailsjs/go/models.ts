efd24a19022fc890740cecc61603dda66eb2b336
metaisfacil <metaisfacil@users.noreply.github.com>
2026-03-19 06:03:58 -0400

    Automatically set corner detection min distance

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -153,6 +153,20 @@ export namespace main {
 	        this.size = source["size"];
 	    }
 	}
+	export class SuggestedCornerParams {
+	    minDistance: number;
+	    maxCorners: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new SuggestedCornerParams(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.minDistance = source["minDistance"];
+	        this.maxCorners = source["maxCorners"];
+	    }
+	}
 	export class ImageInfo {
 	    width: number;
 	    height: number;
@@ -160,6 +174,7 @@ export namespace main {
 	    format: string;
 	    dpiX: number;
 	    dpiY: number;
+	    suggestedCornerParams: SuggestedCornerParams;
 	
 	    static createFrom(source: any = {}) {
 	        return new ImageInfo(source);
@@ -173,7 +188,26 @@ export namespace main {
 	        this.format = source["format"];
 	        this.dpiX = source["dpiX"];
 	        this.dpiY = source["dpiY"];
+	        this.suggestedCornerParams = this.convertValues(source["suggestedCornerParams"], SuggestedCornerParams);
 	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
 	}
 	export class LaunchArgs {
 	    filePath: string;
@@ -375,6 +409,7 @@ export namespace main {
 	        this.angleDeg = source["angleDeg"];
 	    }
 	}
+	
 	export class TouchupSettings {
 	    backend: string;
 	    iopaintUrl: string;
