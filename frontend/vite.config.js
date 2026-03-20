9bcb01c6054728f61b5adf34e54cd9398ace7934
metaisfacil <metaisfacil@users.noreply.github.com>
2026-03-20 15:09:40 -0400

    Fix hot reload Wails/Vite dev workflow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,19 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 200,
+    },
+    hmr: {
+      protocol: 'ws',
+      clientPort: 5173,
+      overlay: true,
+    },
+  },
   build: {
     outDir: 'dist',
     sourcemap: true,
